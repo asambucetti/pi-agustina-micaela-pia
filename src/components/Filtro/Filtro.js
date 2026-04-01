971d9b03e7634a8fa00e2cbb7ee8ed0ba39de950
Micaela Son <mica@MacBook-Pro-7.local>
2026-04-01 16:19:09 -0300

    peliculas y series.js y filtro

---
+++ src/components/Filtro/Filtro.js
@@ -0,0 +1,21 @@
+
+
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+function Filtro(props) {
+  return (
+    <div>
+     <form class="filter-form px-0 mb-3" action="" method="get">
+            <input type="text" name="filter" id="" placeholder="Buscar dentro de la lista"/>
+        </form>
+
+    </div>
+  );
+}
+
+
+export default Filtro;
+
+
+
