842ca6cd013153dba9ccbd64464a327b48e7c80f
Agustina Sambucetti <agustinasambucetti@MacBook-Air-de-Agustina-2.local>
2026-04-06 19:57:26 -0300

    api key para components detalles series y elaboracion card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,31 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-function Card(props) {
-    let sesionExiste = localStorage.getItem("usuario");
+class Card extends Component {
+    constructor() {
+        super();
+        this.state = {
+            mostrar: false,
+        }
+    }
 
-    const apiKey = "5c6cfdfae06798b19907f4b6448f6847"
+    mostrarDescripcion() {
+        this.setState({
+            mostrar: !this.state.mostrar
+        })
+    }
 
-    return (
-        <article className="single-card-movie">
-            <img className="card-img-top" src={props.img} alt={props.titulo} />
-            <div class="cardBody">
-                <h5 className="card-title">{props.titulo}</h5>
-                <p className="card-text">{props.descripcion}</p>
-                {/* aca solo faltaria hacer que la descripcion aparezca y desaparezca (onClick) */}
-                <button>Ver descripción</button>
+    render() {
+        let sesionExiste = localStorage.getItem("usuario");
+        const apiKey = "5c6cfdfae06798b19907f4b6448f6847";
 
-                <Link to={`/detalle/${props.id}`}>
+        return (
+
+        <article className = "single-card-movie" >
+            <img className="card-img-top" src={`https://image.tmdb.org/t/p/w342/${this.props.imagen}`} alt={this.props.titulo} />
+            <div className="cardBody">
+                <h5 className="card-title">{this.props.titulo}</h5>
+                {this.state.mostrar ? <p className="card-text">{this.props.descripcion}</p> : null}
+
+                <button onClick={() => this.mostrarDescripcion()}>Ver descripción</button>
+
+                <Link to={`/detalle/${this.props.id}`}>
                     <button>Ir a detalle</button>
                 </Link>
 
                 {sesionExiste ? <button>⭐</button> : null}
             </div>
         </article>
-    );
+        );
+    }
+
 }
+
 
 export default Card;
 
 
-{/* esta seria la resolucion de card con el api key hecho; comentario de peliculas.js */}
--- src/components/Series/Series.js
+++ src/components/Series/Series.js
@@ -1,50 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Card from '../Card/Card';
 
-function Series() {
-    const series = [
-        {
-            id: 1,
-            titulo: "Binnelanders",
-            descripcion: "A South African Afrikaans soap opera. It is set in and around the fictional private hospital, Binneland Kliniek, in Pretoria, and the storyline follows the trials, trauma and tribulations of the staff and patients of the hospital.",
-            img: "https://image.tmdb.org/t/p/w500/3bzECfllho8PphdYujLUIuhncJD.jpg"
-        },
-        {
-            id: 2,
-            titulo: "When Destiny Brings the Demon",
-            descripcion: "Modern-day office drone Zou Yan accidentally stumbles into the world of cultivation and wakes up as Liao Tingyan, a disciple of the Qinggutian Sect.",
-            img: "https://image.tmdb.org/t/p/w500/15yDwRPWsI75RdJaBVgb4K5m94I.jpg"
-        },
-        {
-            id: 3,
-            titulo: "Sturm der Liebe",
-            descripcion: "These are the stories of relationships taking place in the fictional five-star hotel Fürstenhof, located in Feldkirchen-Westerham near Rosenheim with the plot revolving around members of the family room area, the hotel owners, and employees.",
-            img: "https://image.tmdb.org/t/p/w500/jfFNydakwvbeACEwSd2Gh8UWtba.jpg"
-        },
-        {
-            id: 4,
-            titulo: "Raw",
-            descripcion: "A regularly scheduled, live, year-round program featuring some of the biggest WWE Superstars.",
-            img: "https://image.tmdb.org/t/p/w500/pv5WNnLUo7mpT8k901Lo8UovrqI.jpg"
-        }
-    ];
+class Series extends Component {
 
+    constructor() {
+        super();
+        this.state = {
+            series: []
+        };
+    }
 
-    return (
-        <section>
-            {series.map((serie, idx) => (
-                <Card
-                    key={idx}
-                    id={serie.id}
-                    titulo={serie.titulo}
-                    descripcion={serie.descripcion}
-                    img={serie.img}
-                />
-            )
-            )}
-        </section>
-    );
+    componentDidMount() {
+        const apiKey = "5c6cfdfae06798b19907f4b6448f6847";
 
+        fetch(`https://api.themoviedb.org/3/tv/popular?api_key=${apiKey}`)
+            .then(res => res.json())
+            .then(data => {
+                this.setState({
+                    series: data.results
+                });
+            })
+            .catch(error => console.log(error));
+    }
+
+    render() {
+        return (
+            <section className="row">
+                {this.state.series.map((serie, idx) => (
+                    <Card
+                        key={idx}
+                        id={serie.id}
+                        titulo={serie.name}
+                        descripcion={serie.overview}
+                        imagen={serie.poster_path}
+                    />
+                ))}
+            </section>
+        );
+    }
 }
 
 export default Series;
