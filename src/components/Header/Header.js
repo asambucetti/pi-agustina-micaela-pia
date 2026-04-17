8dc46a941f8ab1d3bb08e09651c0e927493388a6
pivancovich-tech <pivancovich@udesa.edu.ar>
2026-04-17 10:39:05 -0300

    Cambio en Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 function Header() {
@@ -51,7 +52,7 @@ function Header() {
 }
 
 
-export default Header;
+export default withRouter(Header);
 
 
 
