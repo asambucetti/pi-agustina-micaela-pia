d6ef9d400d44b26867697723b1927da6c5cf696e
Micaela Son <mica@MacBook-Pro-7.local>
2026-05-13 17:32:41 -0300

    cambie a funcion logout

--- src/components/Logout/Logout.js
+++ src/components/Logout/Logout.js
@@ -1,33 +1,25 @@
 import React, { Component } from 'react';
 import Cookies from 'universal-cookie';
-import { withRouter } from 'react-router-dom';
 
 const cookies = new Cookies()
 
-class Logout extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    };
+function Logout(props) {
 
 
-    logout() {
+    function logout() {
         cookies.remove('auth-user')
-        this.props.history.push("/Login");
+        props.history.push("/Login");
     };
 
-    render() {
-        return (
-            <button
-                type="submit"
-                className="btn btn-primary btn-block"
-                onClick={(event) => this.logout(event)}
-            >
-                Logout
-            </button>
-        );
-    }
+    return (
+    <button
+        type="submit"
+        className="btn btn-primary btn-block"
+        onClick={logout}
+    >
+        Logout
+    </button>
+);
 }
 
-
-export default withRouter(Logout);
+export default Logout;
